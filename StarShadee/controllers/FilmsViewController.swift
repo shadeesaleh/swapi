b8b2a2148c9dc88bebc28192f1b943ea7bc58599
Shadee Saleh <shadeechuasaleh@gmail.com>
2016-02-17 03:38:29 +0800

    Pods : added Falcon and MRProgress
    Trial tests for Falcon library

--- StarShadee/controllers/FilmsViewController.swift
+++ StarShadee/controllers/FilmsViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import MRProgress
+import Falcon
 
-class FilmsViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+class FilmsViewController: UIViewController/*, UITableViewDataSource, UITableViewDelegate */{
+  
+    
+  @IBOutlet weak var tableView: UITableView!
+  
+  var films:[AnyObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        MRProgressOverlayView.showOverlayAddedTo(self.view, animated: true)
+      
+      //
+      SWAPI.getFilmsWithCompletion({
+          (result:SWResultSet!, error: NSError!) -> () in
+          MRProgressOverlayView.dismissOverlayForView(self.view, animated: false)
+          print("Result : \(result)")
+          print("items : \(result.items)")
+          self.films = result.items
+          self.tableView.reloadData()
+      })
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,9 +39,30 @@ class FilmsViewController: UITableViewController, UITableViewDataSource, UITable
         // Dispose of any resources that can be recreated.
     }
   
+
+  
   
   // MARK: - UITableViewDataSource
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return films.count
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    let row = indexPath.row
+    let cell = tableView.dequeueReusableCellWithIdentifier("FilmTableViewCell", forIndexPath: indexPath) as! FilmTableViewCell
+    
+    let film = films[row] as! SWFilm
+    
+    cell.title.text = film.title
+    cell.subtitle.text = "Episode \(film.episodeID)"
+    
+    
+    return cell;
+  }
+  
   // MARK: - UITableViewDelegate
+
   
 
 
---
+++ StarShadee/models/Film.swift
@@ -0,0 +1,13 @@
+//
+//  Film.swift
+//  StarShadee
+//
+//  Created by Shadee Saleh on 17/02/2016.
+//  Copyright © 2016 SS. All rights reserved.
+//
+
+import Foundation
+
+class Film: NSObject{
+  
+}
